9c5c7586931d4a92f0954e3bb8cd07ccdd743533
Nikolay <pan0v-nikolay@yandex.com>
2023-04-06 21:23:19 +0300

    Добавлены 5 моделей

---
+++ data/classes.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Class(SqlAlchemyBase):
+    __tablename__ = 'classes'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    id_school = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('schools.id'))
+
+    school = orm.relationship('School')
